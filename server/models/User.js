6aca21ed41a7f171b0fb956381e5edad05d09493
sopnendudutta <sopnendu7@gmail.com>
2026-05-16 17:31:33 +0530

    backend error fix

--- server/models/User.js
+++ server/models/User.js
@@ -2,24 +2,19 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    //  ADDED: Username field
-    username: {
-        type: String,
-        required: [true, 'Username is required'],
-        unique: true,
-        trim: true
-    },
+    // 🚨 Username block completely removed
     email: {
         type: String,
         required: [true, 'Email is required'],
         unique: true,
-        trim: true
+        trim: true,
+        lowercase: true
     },
     password: {
         type: String,
         required: [true, 'Password is required'],
         minLength: 8,
-        select: false // this will hide the password by default
+        select: false
     },
     role: {
         type: String,
@@ -28,13 +23,11 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
-// Password encryption before saving 
 userSchema.pre('save', async function () {
     if (!this.isModified('password')) return;
     this.password = await bcrypt.hash(this.password, 12);
 });
 
-// Check if the password is correct
 userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
     return await bcrypt.compare(candidatePassword, userPassword);
 };
